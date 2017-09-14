92694fac6ebdcf648ee865a4bbead8e504b932df
caoyu5779 <caoyu5779@126.com>
2017-09-14 14:48:09 +0800

    * foundation

---
+++ database/migrations/2017_09_14_063013_create_list_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateListTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //创建list表
+        Schema::create('lists', function (Blueprint $table)
+        {
+            $table->increments('id');
+            $table->string('title');
+            $table->string('author');
+            $table->text('body')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
